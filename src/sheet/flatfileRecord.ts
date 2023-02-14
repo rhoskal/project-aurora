008f75b612e5e935ba11923a7a3303245d261f94
Hans Hoffman <hans@flatfile.io>
2023-02-14 12:09:38 -0700

    update with function overloading

--- src/sheet/flatfileRecord.ts
+++ src/sheet/flatfileRecord.ts
@@ -1,7 +1,9 @@
+import * as G from "../helpers/typeGuards";
+
 export interface FlatfileRecord {
   get(key: string): unknown;
   set(key: string, value: string | number | object): this;
-  // modify(key: string, handler: (value) => value): null | string | number | object;
+  modify(key: string, handler: (value: unknown) => typeof value): this;
   addInfo(key: string, message: string): this;
   addInfo(keys: Array<string>, message: string): this;
   addWarning(key: string, message: string): this;
@@ -10,21 +12,101 @@ export interface FlatfileRecord {
   addError(keys: Array<string>, message: string): this;
 }
 
-// export class DataRecord implements FlatfileRecord<T> {
-//   private value: unknown;
+export class DataRecord implements FlatfileRecord {
+  private value: Record<string, unknown>;
 
-//   constructor() {
-//     this.value = null;
-//   }
+  constructor(record: Record<string, unknown>) {
+    this.value = record;
+  }
 
-//   get(key: string) { }
+  /**
+   * Get a field value.
+   *
+   * @param {string} key
+   * @returns unknown
+   */
+  get(key: string) {
+    return this.value[key];
+  }
 
-//   set(key: string) {
-//     return this;
-//   }
+  /**
+   * Set a field value.
+   *
+   * @param {string} key
+   * @param {(string|number|object)} value
+   * @returns this
+   */
+  set(key: string, value: unknown) {
+    this.value[key] = value;
 
-//   addInfo(key: string, message: string): this
-//   addInfo(keys: Array<string>, message: string): this {
-//     return this;
-//   }
-// }
+    return this;
+  }
+
+  /**
+   * Get & set a field value at the same time.
+   *
+   * @param {string} key
+   * @param {function} handler
+   * @returns this
+   */
+  modify(key: string, handler: (value: unknown) => typeof value): this {
+    const currentValue = this.value[key];
+    this.value[key] = handler(currentValue);
+
+    return this;
+  }
+
+  /**
+   * Add an info annotation to a cell/cells.
+   *
+   * @param {(string|string[])} key
+   * @param {string} message
+   */
+  addInfo(key: string, message: string): this;
+  addInfo(keys: Array<string>, message: string): this;
+  addInfo(key: unknown, message: string): this {
+    if (G.isString(key)) {
+      // do something
+    } else {
+      // do something
+    }
+
+    return this;
+  }
+
+  /**
+   * Add an info annotation to a cell/cells.
+   *
+   * @param {(string|string[])} key
+   * @param {string} message
+   */
+  addWarning(key: string, message: string): this;
+  addWarning(keys: Array<string>, message: string): this;
+  addWarning(key: unknown, message: string): this {
+    if (G.isString(key)) {
+      // do something
+    } else {
+      // do something
+    }
+
+    return this;
+  }
+
+  /**
+   * Add an error annotation to a cell/cells.
+   *
+   * @param {(string|string[])} key
+   * @param {string} message
+   */
+  addError(key: string, message: string): this;
+  addError(keys: Array<string>, message: string): this;
+  addError(key: unknown, message: string): this {
+    if (G.isString(key)) {
+      // do something
+    } else {
+      // do something
+    }
+
+    return this;
+  }
+}
